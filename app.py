297d281b94348a4e016f1db96b47f7ea6e4f4bfb
Abraham Ho <whoabe@Abrahams-MacBook-Air.local>
2019-06-17 12:09:01 +0800

    user sign up added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import config
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
 from models.base_model import db
+from models.user import User
+import peeweedbevolve
 
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'instagram_web')
@@ -25,3 +27,25 @@ def _db_close(exc):
         print(db)
         print(db.close())
     return exc
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+        
+@app.route("/new")
+def x():
+    return render_template('new.html')
+
+@app.route("/new_form", methods = ["POST"])
+def create():
+    data = User(username=request.form['username'], email=request.form['email'], password = request.form['password'])
+
+    if data.save():
+        flash("flash saved")
+        return redirect(url_for('x'))
+        #goes to function x
+    else:
+        return render_template('new.html', username = request.form['username'], email =request.form['email'], password = request.form['password'])
+
+
--- models/user.py
+++ models/user.py
@@ -3,4 +3,6 @@ import peewee as pw
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique = True, null = False)
+    email = pw.CharField(unique=False, null = False)
+    password = pw.CharField(unique = False, null = False)
